07318f2eb2f3486a2c81d79f6c1d92edad089d1a
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-10-04 18:36:12 +0100

    chore: added explore page folder to pages

---
+++ pages/explore/index.tsx
@@ -0,0 +1,12 @@
+// import Link from 'next/link';
+import MainLayout from '../../components/Layout/MainLayout';
+
+function Home() {
+  return (
+    <MainLayout activePage="home">
+      <p className="text-dark-100">Explore</p>
+    </MainLayout>
+  );
+}
+
+export default Home;
